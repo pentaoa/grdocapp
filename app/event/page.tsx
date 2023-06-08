5f9260f62d8ba4b0fa01d99be6c8e740c9b523a5
Grada <grada23333@outlook.com>
2023-06-08 20:56:28 +0800

    🐛 Bug Fix

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -9,18 +9,15 @@ export default function AboutPage() {
   return (
     <section>
       <h1 className="font-bold text-4xl my-4">特殊页面</h1>
-      <div className="my-4 text-neutral-800 dark:text-neutral-200">
-          回看天际下中流，岩上无心云相逐。
-      </div>
       
-      <article className="group my-4">
-      <Link href="/blog/establish"> 
+      <div className="group my-6">
+        <Link href="/blog/establish">
         <img
         alt="Reading"
         src="https://ihs-1317591693.cos.ap-guangzhou.myqcloud.com/public/small10.jpg"
         className="cursor-none h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:drop-shadow-2xl"
         />
-
+        </Link>
       <div className="p-4">
           <h3 className="text-lg font-medium text-gray-900">
             建站小记
@@ -29,17 +26,16 @@ export default function AboutPage() {
         在写这个网站的前一个网站的时候写的文章。时间大概在四月左右，我开始了建站的日程。
         </div>
       </div>
-      </Link>
-      </article>
+      </div>
 
-      <article className="group my-4">
+      <div className="group my-6">
       <a href="https://github.com/pentaoa/grdocapp/commit/main"> 
         <img
         alt="Macos"
         src="https://ihs-1317591693.cos.ap-guangzhou.myqcloud.com/public/big6.jpg"
         className="cursor-none h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:drop-shadow-2xl"
         />
-
+      </a>
       <div className="p-4">
           <h3 className="text-lg font-medium text-gray-900">
             更新日志
@@ -48,26 +44,24 @@ export default function AboutPage() {
         网站的各个时间节点
         </div>
       </div>
-      </a>
-      </article>
-
-      <div className="group my-4">
-      <link href="https://github.com/pentaoa/grdocapp/commit/main"> 
-        <img
-        alt="MC"
-        src="https://ihs-1317591693.cos.ap-guangzhou.myqcloud.com/grdoc/MCV-Trails-and-Tales.jpg"
-        className="cursor-none h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:drop-shadow-2xl"
-        />
-
-      <div className="p-4">
-          <h3 className="text-lg font-medium text-gray-900">
-            Minecraft 服务器
-          </h3>
-        <div className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-        欢迎来到我的 MC 私人服务器——PtaWorld，该服务器无需正版验证。是一个原版生存服务器，欢迎你的到来。
-        </div>
       </div>
-      </link>
+
+      <div className="group my-6">
+        <a href="https://pixelcloud.cn/servers/6481c59e05bed9002d272fae"> 
+          <img
+            src="https://ihs-1317591693.cos.ap-guangzhou.myqcloud.com/grdoc/MCV-Trails-and-Tales-sak.webp"
+            className="cursor-none h-56 w-full rounded-xl object-cover shadow-xl transition group-hover:drop-shadow-2xl"
+          />
+        </a>
+          <div className="p-4">
+            <div className="text-lg font-medium text-gray-900">
+              Minecraft 服务器
+            </div>
+            <div className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+              欢迎来到我的 MC 私人服务器——PtaWorld，该服务器无需正版验证。是一个原版生存服务器，欢迎你的到来。
+            </div>
+          </div>
+
       </div>
     </section>
   );
